3cf4d65316d96f41bac5cdbd51dac7eea142f49a
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-07-29 18:35:16 +0530

    04- Authentication

---
+++ app/api/users/route.ts
@@ -0,0 +1,38 @@
+import { db } from "@/config/db";
+import { usersTable } from "@/config/schema";
+import { currentUser } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const user = await currentUser();
+
+  try {
+    const users = await db
+      .select()
+      .from(usersTable)
+      //@ts-expect-error ERROR
+      .where(eq(usersTable?.email, user?.primaryEmailAddress?.emailAddress));
+
+    if (users?.length == 0) {
+      const result = await db
+        .insert(usersTable)
+           //@ts-expect-error ERROR
+
+        .values({
+          name: user?.fullName ?? "Anonymous",
+          email: user?.primaryEmailAddress?.emailAddress,
+          credits: 10,
+        })
+              //@ts-expect-error ERROR
+
+        .returning({usersTable});
+
+      return NextResponse.json(result[0]?.usersTable);
+    }
+
+    return NextResponse.json(users[0]);
+  } catch (error) {
+    return NextResponse.json(error);
+  }
+}
